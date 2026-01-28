161b69ba2ea991e514ad282c5dbfc3399a055f2d
Rajath-Thaliyadath <rajath.thaliyadath@students.mq.edu.au>
2026-01-28 22:15:49 +1100

    Slow down all animations for smoother, more elegant feel

--- static/script.js
+++ static/script.js
@@ -22,10 +22,10 @@ function animateOnLoad() {
         el.style.opacity = '0';
         el.style.transform = 'translateY(20px)';
         setTimeout(() => {
-            el.style.transition = 'opacity 0.6s ease, transform 0.6s ease';
+            el.style.transition = 'opacity 1s ease, transform 1s ease';
             el.style.opacity = '1';
             el.style.transform = 'translateY(0)';
-        }, index * 100);
+        }, index * 200);
     });
 }
 
